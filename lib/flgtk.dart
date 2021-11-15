dfd3bdfe39ce25d2a1f23a981ee85e5b4bb1c76e
simrat39 <simrats169169@gmail.com>
2021-11-15 13:40:41 -0800

    refactor: Move GtkButtonThemeData to its own module

--- lib/flgtk.dart
+++ lib/flgtk.dart
@@ -2,43 +2,9 @@
 
 import 'dart:async';
 
+import 'package:flgtk/gtk_button_theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-class GtkButtonThemeData {
-  final int normal_bg;
-  final int normal_fg;
-
-  final int active_bg;
-  final int active_fg;
-
-  final int prelight_bg;
-  final int prelight_fg;
-
-  final int selected_bg;
-  final int selected_fg;
-
-  final int focused_bg;
-  final int focused_fg;
-
-  final int checked_bg;
-  final int checked_fg;
-
-  const GtkButtonThemeData({
-    required this.normal_bg,
-    required this.normal_fg,
-    required this.active_bg,
-    required this.active_fg,
-    required this.prelight_bg,
-    required this.prelight_fg,
-    required this.selected_bg,
-    required this.selected_fg,
-    required this.focused_bg,
-    required this.focused_fg,
-    required this.checked_bg,
-    required this.checked_fg,
-  });
-}
 
 class GtkThemeData {
   static const MethodChannel _channel = MethodChannel('flgtk');
---
+++ lib/gtk_button_theme_data.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: non_constant_identifier_names
+
+class GtkButtonThemeData {
+  final int normal_bg;
+  final int normal_fg;
+
+  final int active_bg;
+  final int active_fg;
+
+  final int prelight_bg;
+  final int prelight_fg;
+
+  final int selected_bg;
+  final int selected_fg;
+
+  final int focused_bg;
+  final int focused_fg;
+
+  final int checked_bg;
+  final int checked_fg;
+
+  const GtkButtonThemeData({
+    required this.normal_bg,
+    required this.normal_fg,
+    required this.active_bg,
+    required this.active_fg,
+    required this.prelight_bg,
+    required this.prelight_fg,
+    required this.selected_bg,
+    required this.selected_fg,
+    required this.focused_bg,
+    required this.focused_fg,
+    required this.checked_bg,
+    required this.checked_fg,
+  });
+}
