5f629b0944f93aa570ad56ef7c771607ed057879
Simrat Grewal <simrats169169@gmail.com>
2022-02-20 20:09:15 -0800

    docs: Add doc comments

--- lib/gtk_theme_fl.dart
+++ lib/gtk_theme_fl.dart
@@ -2,25 +2,43 @@
 
 import 'dart:async';
 
-import 'package:gtk_theme_fl/gtk_button_theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class GtkThemeData {
   static const MethodChannel _channel = MethodChannel('gtk_theme_fl');
 
+  /// Name of the current GTK Theme.
   String name;
+
+  /// Foreground Color, usually for text.
   int theme_fg_color;
+
+  /// Text Color.
   int theme_text_color;
+
+  /// Main BG Color.
   int theme_bg_color;
+
+  /// Main Base Color.
   int theme_base_color;
+
+  /// Main Selected BG Color.
   int theme_selected_bg_color;
+
+  /// Main selected FG Color.
   int theme_selected_fg_color;
 
+  /// Usually orange / yellow on most themes
   int warning_color;
+
+  /// Usually red on most themes
   int error_color;
+
+  /// Usually blue / green on most themes
   int success_color;
 
+  /// Current font with size, might need parsing
   String font;
 
   GtkThemeData({
@@ -41,10 +59,12 @@ class GtkThemeData {
         scaffoldBackgroundColor: Color(theme_base_color),
       );
 
+  /// Returns the raw json data from gtk
   static Future<Map<Object?, Object?>> _getRawData() async {
     return await _channel.invokeMethod('getThemeData');
   }
 
+  /// Initialize a [GtkThemeData] object with the current theme data.
   static Future<GtkThemeData> initialize() async {
     var data = await _getRawData();
 
